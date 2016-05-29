8311e977ca9bf9fafadfdb996ba2b185351b7d2a
Matt Pasco <matt.pasco@hotmail.com>
2016-05-29 17:20:20 -0400

    http

--- import.js
+++
@@ -1,9 +0,0 @@
-
-  function updateMe(var filename){
-    var x = 0;
-    var activity=jsonstr;
-    foreach (i in activity) {
-        date = document.getElementById(i.date).innerHTML = activity.date;
-        event = document.getElementById(i.event).innerHTML = activity.event;
-    }
-  }
