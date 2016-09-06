1dc3d7499757bba25676564fe6819d9834590161
Matt Pasco <matt.pasco@hotmail.com>
2016-09-06 17:52:55 -0400

    Revert "removed redundant files"
    This reverts commit a3016164aecfce47f6b178ec03a264a173e1016f.

---
+++ methods.js
@@ -0,0 +1,47 @@
+var app = angular.module('myApp', []);
+app.filter('filterByTags', function () {
+    return function (items, key) {
+        var filtered = [];
+        console.log(items);
+        angular.forEach(items, function (item) {
+
+            var contains = false;
+            angular.forEach(item.tags, function (tag) {
+                if (tag.toLowerCase().indexOf(key.toLowerCase()) > -1 || key.toLowerCase() == "all")
+                    contains = true;
+            });
+            if (contains == true)
+                filtered.push(item);
+        });
+        console.log(filtered);
+        return filtered;
+
+    };
+});
+app.filter('filterByDifficulty', function () {
+    return function (items, key) {
+        var filtered = [];
+
+        angular.forEach(items, function (item) {
+            if (item.difficulty <= key)
+                filtered.push(item);
+        });
+        return filtered;
+
+    };
+});
+
+
+app.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.tag = "ALL";
+    $scope.difficulty = 5;
+    $http({
+        method: "GET",
+        url: "../Data/data.json"
+    }).then(function mySuccess(response) {
+        $scope.data = response.data;
+    }, function myError(response) {
+        console.log("error");
+    });
+	}]);
+
